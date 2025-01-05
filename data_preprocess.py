32e1d57fa2de753d0c16fd0034dd9dea3a153b2f
zhao <zhaozicheng1212@gmail.com>
2025-01-05 23:03:53 +0900

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -14,37 +14,30 @@ def data_clean(batch_size=10000):
             try:
                 content = obj.get('text', '')
                 if len(content) > 512:
-                    # 找到512字符以内的部分
+         
                     truncated_content = content[:512]
-                    # 找到最后一个句号的位置
                     last_period_index = truncated_content.rfind('。')
                     if last_period_index != -1:
-                        # 截取最后一个句号之前的内容
                         content = truncated_content[:last_period_index + 1]
                     else:
-                        # 如果没有句号，直接截取512字符
                         content = truncated_content
 
                 text_id = tokenizer(f'{bos_token}{content}{eos_token}').input_ids
                 batch.extend(text_id)
                 data_available += 1
 
-                # 当达到批次大小时，写入文件
                 if len(batch) >= batch_size:
                     arr = np.array(batch, dtype=np.uint16)
                     with open('./pretrain_data.bin', 'ab') as f:
                         f.write(arr.tobytes())
                     batch.clear()
                     print(f"Processed {idx + 1} records and appended to pretrain_data.bin")
-
-                # 每处理50000条记录打印一次进度
                 if idx % 50000 == 0:
                     print(f"seq_monkey: [{idx}]")
             except UnicodeDecodeError as e:
                 print(f"Skipping invalid line {idx + 1}: {e}")
                 continue
 
-    # 处理最后一批次数据
     if batch:
         arr = np.array(batch, dtype=np.uint16)
         with open('./pretrain_data.bin', 'ab') as f:
@@ -56,7 +49,6 @@ def data_clean(batch_size=10000):
 def pretrain_process():
     data_clean()
 
-    # 合并所有数据文件（如果有多个）
     data_path_list = ['./pretrain_data.bin']
     data_lst = []
     for data_path in data_path_list:
