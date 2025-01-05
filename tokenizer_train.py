b394f97a3f0b46d0502c290b625dde77c3e17f57
zhao <zhaozicheng1212@gmail.com>
2025-01-05 23:02:41 +0900

    Update tokenizer_train.py

--- tokenizer_train.py
+++ tokenizer_train.py
@@ -1,9 +1,14 @@
 import random
+from tqdm import tqdm
+from transformers import AutoTokenizer
 import json
+from datasets import load_dataset
 from tokenizers import (
     decoders,
     models,
+    normalizers,
     pre_tokenizers,
+    processors,
     trainers,
     Tokenizer,
 )
@@ -12,38 +17,51 @@ import os
 random.seed(42)
 
 def train_tokenizer():
+    # 读取JSONL文件并提取文本数据
     def read_texts_from_jsonl(file_path):
         with open(file_path, 'r', encoding='utf-8') as f:
             for line in f:
                 data = json.loads(line)
                 yield data['text']
 
-    data_path = 'tokenizer_train.jsonl'
+    data_path = 'train_0.jsonl'
 
+    # 初始化tokenizer
     tokenizer = Tokenizer(models.BPE())
     tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=False)
+
+    # 定义特殊token
     special_tokens = ["<unk>", "<s>", "</s>"]
 
+    # 设置训练器并添加特殊token
     trainer = trainers.BpeTrainer(
-        vocab_size=6000,
-        special_tokens=special_tokens,  
+        vocab_size=7000,
+        special_tokens=special_tokens,  # 确保这三个token被包含
         show_progress=True,
         initial_alphabet=pre_tokenizers.ByteLevel.alphabet()
     )
 
+    # 读取文本数据
     texts = read_texts_from_jsonl(data_path)
+
+    # 训练tokenizer
     tokenizer.train_from_iterator(texts, trainer=trainer)
 
+    # 设置解码器
     tokenizer.decoder = decoders.ByteLevel()
+
+    # 检查特殊token的索引
     assert tokenizer.token_to_id("<unk>") == 0
     assert tokenizer.token_to_id("<s>") == 1
     assert tokenizer.token_to_id("</s>") == 2
 
-    tokenizer_dir = "./my_tokenizer"
+    # 保存tokenizer
+    tokenizer_dir = "./model/jp7000_tokenizer"
     os.makedirs(tokenizer_dir, exist_ok=True)
     tokenizer.save(os.path.join(tokenizer_dir, "tokenizer.json"))
-    tokenizer.model.save("./my_tokenizer")
+    tokenizer.model.save("./model/jp7000_tokenizer")
 
+    # 手动创建配置文件
     config = {
         "add_bos_token": False,
         "add_eos_token": False,
@@ -86,19 +104,17 @@ def train_tokenizer():
         "tokenizer_class": "PreTrainedTokenizerFast",
         "unk_token": "<unk>",
         "use_default_system_prompt": False,
-        "chat_template": "{% if messages[0]['role'] == 'system' %}{% set system_message = messages[0]['content'] %}{% endif %}{% if system_message is defined %}{{ system_message }}{% endif %}{% for message in messages %}{% set content = message['content'] %}{% if message['role'] == 'user' %}{{ '<s>user\n' + content + '</s>\n<s>assistant\n' }}{% elif message['role'] == 'assistant' %}{{ content + '</s>' + '\n' }}{% endif %}{% endfor %}"
+        "chat_template": "{% if messages[0]['role'] == 'system' %}{% set system_message = messages[0]['content'] %}{% endif %}{% if system_message is defined %}{{ system_message }}{% endif %}{% for message in messages %}{% set content = message['content'] %}{% if message['role'] == 'user' %}{{ '<s>user\\n' + content + '</s>\\n<s>assistant\\n' }}{% elif message['role'] == 'assistant' %}{{ content + '</s>' + '\\n' }}{% endif %}{% endfor %}"
     }
 
+    # 保存配置文件
     with open(os.path.join(tokenizer_dir, "tokenizer_config.json"), "w", encoding="utf-8") as config_file:
         json.dump(config, config_file, ensure_ascii=False, indent=4)
 
     print("Tokenizer training completed and saved.")
 
-
-
 def main():
     train_tokenizer()
-    # eval_tokenizer()
-
+    
 if __name__ == '__main__':
     main()
